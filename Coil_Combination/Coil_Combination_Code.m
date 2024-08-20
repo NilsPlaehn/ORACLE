1682d32284f78ffc09e2ee5fe4e493e6ed60d9a8
NilsPlaehn <102172185+NilsPlaehn@users.noreply.github.com>
2024-08-20 09:46:00 +0200

    Update Coil_Combination_Code.m

--- Coil_Combination/Coil_Combination_Code.m
+++ Coil_Combination/Coil_Combination_Code.m
@@ -15,13 +15,6 @@
 
 
 
-% Author of function: 
-% Nils MJ Plähn, Bern, Switzerland
-% E-mail: nils.plaehn@students.unibe.ch
-% Department of Diagnostic, Interventional and Pediatric Radiology (DIPR), Inselspital, Bern University Hospital, University of Bern, Switzerland
-% Translation Imaging Center (TIC), Swiss Institute for Translational and Entrepreneurial Medicine, Bern, Switzerland
-
-
 clear all
 
 
