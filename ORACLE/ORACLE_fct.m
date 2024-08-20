c11796f10e62c88db9654a2de4e6d2231b874ed7
NilsPlaehn <102172185+NilsPlaehn@users.noreply.github.com>
2024-08-20 09:38:33 +0200

    Update ORACLE_fct.m

--- ORACLE/ORACLE_fct.m
+++ ORACLE/ORACLE_fct.m
@@ -1,4 +1,5 @@
 % Description: ORACLE framework
+% This code is for research purposes only.
 
 % Author of function: 
 % Nils MJ Plähn, Bern, Switzerland
@@ -6,41 +7,48 @@
 % Department of Diagnostic, Interventional and Pediatric Radiology (DIPR), Inselspital, Bern University Hospital, University of Bern, Switzerland
 % Translation Imaging Center (TIC), Swiss Institute for Translational and Entrepreneurial Medicine, Bern, Switzerland
 
+ 
 
 function [T1_est,T2_est, theta,M0] = ORACLE_fct(bSSFPSignal,TR,alpha)
+    % uniformly distributed RF phase increments
     NPhaseCycle = numel(bSSFPSignal);
     phi_tmp     = linspace(0,2*pi,NPhaseCycle+1); 
     phi         = phi_tmp(1:NPhaseCycle);      
    
-    % VZ depends on the handedness of the coordinate system
+    % VZ depends on the handedness of the coordinate system (Right handed coordinate system has VZ=1 and lefthanded VZ=-1)
     VZ = 1; 
-
+    % get central modes (alternative is to pass them to this function if aliasing correction is used)
     cm1 = conj(NPointFT(bSSFPSignal,-1.*VZ,phi));
     c0  = NPointFT(bSSFPSignal,0.*VZ,phi);
     c1  = NPointFT(bSSFPSignal,1.*VZ,phi);
 
-    z       = c1/c0;
-    theta   = angle(z);
-
-    A        = c0;
-    
+    % Auxilliary definitions 1
+    z        = c1/c0;
+    A        = c0; 
     B        = cm1/z;
- 
-    q  = abs(A)./abs(B);
+    % Auxilliary definitions 2 
+    x   = abs(A)./abs(B);
+    r   = abs(z);   
 
-    r   = abs(z);
-    
-    M0 = abs(A)+abs(B);
+   
+    % T2 quantification
+    E2      = (1+x).*r./(r^2+x);
+    T2_est  = -TR./log(E2);
 
-    E2      = (1+q).*r./(r^2+q);
-    T2_est = -TR./log(E2);
-
-    Zahler  = E2-2*r+E2*r^2+E2*(1-2*E2*r+r^2)*cos(alpha);   % Definition 1
-    Nenner  = E2*(1-2*E2*r+r^2)+(E2-2*r+E2*r^2)*cos(alpha); % Definition 2
-    E1      = Zahler/Nenner; 
+    % T1 quantification
+    a = E2-2*r+E2*r^2;
+    b = E2*(1-2*E2*r+r^2);
+    E1      = (a+b*cos(alpha))/(b+a*cos(alpha)); 
     T1_est  = -TR./log(E1);
+    
+    % PD quantification    
+    M0  = abs(A)+abs(B);
+    M0  = M0/(2*tan(alpha/2))*exp(TR/T2_est/2);
 
-    M0 = M0/(2*tan(alpha/2))*exp(TR/T2_est/2);
+    % B0 inhomogeneity/off-resonance quantification
+    theta    = angle(z);
+    % if df or dB desired by divide by "2*pi*TR" or "gamma*TR" respectively
+    
 end
 
 
